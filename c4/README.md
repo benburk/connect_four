805803cc00f1aa97a8e8f973aae3210fdcbd9743
Ben Burk <benburk@outlook.com>
2021-09-04 18:59:14 -0500

    Add files via upload

---
+++ c4/README.md
@@ -0,0 +1,24 @@
+https://github.com/lhorrell99/Connect4Solver
+- connect 4 solver in python
+
+
+Ideas
+- https://www.chessprogramming.org/Unmake_Move
+- Iterative deepening
+- http://web.mit.edu/sp.268/www/2010/connectFourSlides.pdf
+
+
+## Lookup table
+- use board symmetry to cut in half
+- use 6 "sets" - avoid encoding the actual moves
+- use a tree
+
+
+
+
+
+32164625 -> 22 - 8/2 - 11 = 7
+6146 -> 22 - 4/2 - 18 = 2
+243335424257 -> 22 - 12/2 - 12 = 4
+5512243243536 -> 22 - 13/2 - 13 = 2.5=3
+7343363417254 -> 22 - 13/2 - 14 = 2
---
+++ c4/solver.py
@@ -0,0 +1,116 @@
+import math
+from collections import namedtuple
+
+from connect_four import Board
+
+visits = 0
+
+
+def negamax(board: Board, depth: int) -> float:
+    global visits
+    visits += 1
+
+    if depth == 0 or board.is_over():
+        return board.score()
+
+    value = -math.inf
+    for child in board.children():
+        value = max(value, -negamax(child, depth - 1))
+    return value
+
+
+def negabeta(board: Board, alpha: int, beta: int, depth: int) -> int:
+    global visits
+    visits += 1
+
+    if depth == 0 or board.is_over():
+        return board.score()
+
+    value = -math.inf
+    for child in board.children():
+        value = max(value, -negabeta(child, -beta, -alpha, depth - 1))
+        alpha = max(alpha, value)
+        if alpha >= beta:
+            break
+    return value
+
+
+Entry = namedtuple("Entry", "value flag depth")
+
+table = {}
+
+
+def transposition(board: Board, alpha: int, beta: int, depth: int) -> int:
+    global visits
+    visits += 1
+
+    alpha_original = alpha
+
+    if (entry := table.get(board)) and entry.depth >= depth:
+        if entry.flag == "exact":
+            return entry.value
+        if entry.flag == "lower":
+            alpha = max(alpha, entry.value)
+        elif entry.flag == "upper":
+            beta = min(beta, entry.value)
+        if alpha >= beta:
+            return entry.value
+
+    if depth == 0 or board.is_over():
+        return board.score()
+
+    value = -math.inf
+    for child in board.children():
+        value = max(value, -negabeta(child, -beta, -alpha, depth - 1))
+        alpha = max(alpha, value)
+        if alpha >= beta:
+            break
+
+    if value <= alpha_original:
+        table[board] = Entry(value, "upper", depth)
+    elif value >= beta:
+        table[board] = Entry(value, "lower", depth)
+    else:
+        table[board] = Entry(value, "exact", depth)
+
+    return value
+
+
+def main() -> None:
+    """Main method."""
+    global visits
+    board = Board.from_string("34544455332525534114373777771")
+    board = Board.from_string("32164625366433")
+    board = Board.from_string("243335424257")
+
+    print(board)
+
+    # visits = 0
+    # score = negamax(board, 8)
+    # print(f"Negamax: score: {score}, visits: {visits}")
+
+    visits = 0
+    score = negabeta(board, -1, 1, 8)
+    print(f"Negabeta: score: {score}, visits: {visits}")
+
+    visits = 0
+    score = transposition(board, -100, 100, 8)
+    print(f"Transposition: score: {score}, visits: {visits}")
+
+
+def test():
+    # test all the R1
+    test_files = ["Test_L1_R1", "Test_L2_R1", "Test_L3_R1"]
+    for test_file in test_files:
+        with open(f"test_cases/{test_file}", encoding="utf-8") as file:
+            for line in file:
+                moves, score = line.split()
+                board = Board.from_string(moves)
+
+                score = negabeta(board, -1, 1, 8)
+                print(score)
+
+
+if __name__ == "__main__":
+    # test()
+    main()
